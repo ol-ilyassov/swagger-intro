62d7125a5e73a8f8e3cab32593c5e9e5469a5279
Olzhas <olzhas.ilyassov@alg-in.kz>
2023-04-12 17:16:20 +0600

    upload base files

---
+++ cmd/api/movies.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"net/http"
+	"swagger-intro/internal/data"
+	"swagger-intro/internal/validator"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	var output struct {
+		Title  string `json:"title"`
+		Action string `json:"action"`
+	}
+	output.Title = input.Title
+	output.Action = "create success"
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": output}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
+		return
+	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module swagger-intro
+
+go 1.20
+
+require github.com/julienschmidt/httprouter v1.3.0
